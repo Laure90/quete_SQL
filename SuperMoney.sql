17e0356da99e67549cfbeab0fa4978eb01f9e26a
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-02-03 12:26:36 +0100

    Add files via upload

---
+++ SuperMoney.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE SuperMoney;
+GO
+USE SuperMoney;
+CREATE TABLE logs ("id" INT PRIMARY KEY IDENTITY(1,1), "user" VARCHAR(50), "message" VARCHAR(50));
+INSERT INTO logs ("user","message") VALUES ('hack3r', 'Connects');
+INSERT INTO logs ("user","message") VALUES ('hack3r', 'Connects');
+INSERT INTO logs ("user","message") VALUES ('hack3r' , 'Erases file'); 
+INSERT INTO logs ("user","message") VALUES ('hack3r', 'Connects');
+INSERT INTO logs ("user","message") VALUES ('hack3r', 'Create new file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Leaves');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Connects');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Erases file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Create new file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Create new file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Leaves');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Create new file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Create new file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Erases file');
+INSERT INTO logs ("user","message") VALUES ('hack3r','Create new file');
+INSERT INTO logs ("user","message") VALUES ('admin','Leaves');
+INSERT INTO logs ("user","message") VALUES ('admin','Create new file');
+INSERT INTO logs ("user","message") VALUES ('admin','Create new file');
+INSERT INTO logs ("user","message") VALUES ('admin','Create new file');
+INSERT INTO logs ("user","message") VALUES ('admin','Connects');
+INSERT INTO logs ("user","message") VALUES ('admin','Connects');
+INSERT INTO logs ("user","message") VALUES ('admin','Leaves');
+INSERT INTO logs ("user","message") VALUES ('admin','Leaves');
+INSERT INTO logs ("user","message") VALUES ('admin','Erases file');
+INSERT INTO logs ("user","message") VALUES ('admin','Connects');
+INSERT INTO logs ("user","message") VALUES ('admin','Connects');
+INSERT INTO logs ("user","message") VALUES ('admin','Erases file');
+INSERT INTO logs ("user","message") VALUES ('admin','Connects');
+INSERT INTO logs ("user","message") VALUES ('admin','Leaves');
+INSERT INTO logs ("user","message") VALUES ('admin','Create new file');
+
+USE SuperMoney;
+GO
+BACKUP DATABASE SuperMoney
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\Backup\SuperMoneyBackup.bak'
+   WITH FORMAT,
+      MEDIANAME = 'SuperMoneyBackups',
+      NAME = 'Full Backup of SuperMoney';
+GO
+
+USE SuperMoney;
+GO
+BEGIN TRANSACTION;
+DELETE FROM logs WHERE user = 'h4ck3r';
+DELETE TOP(3) FROM logs;
+COMMIT
